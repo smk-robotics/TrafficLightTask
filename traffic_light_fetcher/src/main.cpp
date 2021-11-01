9e4ee737b0d375c3bd8ca084b816b2ecf8bfcb72
Kirill <kirill.smirnov@evocargo.com>
2021-11-01 23:56:50 +0300

    UPD: Update traffic light fetcher launch file.

--- traffic_light_fetcher/src/main.cpp
+++ traffic_light_fetcher/src/main.cpp
@@ -6,7 +6,7 @@
  * @copyright GNU General Public License v3.0.
  * 
  */
-#include "traffic_light_fetcher.h"
+#include "traffic_light_fetcher_node.h"
 #include <iostream>
 
 using namespace traffic_light_fetcher;
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
   try {
     ros::init(argc, argv, "traffic_light_fetcher");
     ros::NodeHandlePtr pnh(new ros::NodeHandle("~"));
-    TrafficLightFetcher traffic_light_fetcher(pnh);
+    TrafficLightFetcherNode traffic_light_fetcher_node(pnh);
     ros::spin();
   } catch (ros::Exception& e) {  // Handle ROS exceptions.
     std::cerr << "[ERROR]:[traffic_light_fetcher] - " << e.what() << std::endl;
